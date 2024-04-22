059cc550546a876ca47d8664098376667006cde8
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-04-22 14:06:31 +0530

    added openai integration

---
+++ Solver/connect.py
@@ -0,0 +1,16 @@
+import openai
+from .config import API_KEY,API_BASE
+openai.api_key = API_KEY
+#openai.api_base =API_BASE
+
+# change engine and remove api_base if using official openai api
+
+#   increase max_tokens to get a long response
+
+def chat(prompt):
+    response = openai.Completion.create(
+        engine="pai-001-light",
+        prompt=prompt,
+        max_tokens=150
+    )
+    return response.choices[0].text.strip()
---
+++ main.py
@@ -0,0 +1,3 @@
+from Solver import connect,input,seggregate
+
+result=connect.chat("request")
