f6ce3494937281e046883ad3a2c2453f74a35295
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-04-25 20:12:25 +0530

    added gpt forward

--- Solver/connect.py
+++ Solver/connect.py
@@ -1,7 +1,7 @@
 import openai
 from .config import API_KEY,API_BASE
 openai.api_key = API_KEY
-#openai.api_base =API_BASE
+openai.api_base =API_BASE
 
 # change engine and remove api_base if using official openai api
 
@@ -9,7 +9,7 @@ openai.api_key = API_KEY
 
 def chat(prompt):
     response = openai.Completion.create(
-        engine="pai-001-light",
+        engine="pai-001",
         prompt=prompt,
         max_tokens=150
     )
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from Solver import connect,input,seggregate
+from flask import Flask,request,jsonify,json
 
-result=connect.chat("request")
+app = Flask(__name__)
+
+
+@app.route("/sendquestion", methods=['GET', 'POST'])
+def solver():
+    if request.method == 'POST':
+        json_data = request.get_json()
+
+        if json_data:
+
+            received_dict = json.loads(json_data)
+            input.sent_prompt(received_dict)
+            return jsonify({"message": "Data received successfully"}), 200
+        else:
+            return jsonify({"error": "No JSON data received"}), 400
+    else:
+        return jsonify({"message": "Send a POST request with JSON data"}), 405
+
+if __name__ == '__main__':
+    app.run(debug=True)
